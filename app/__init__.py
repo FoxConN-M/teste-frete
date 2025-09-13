cc9bff71a2fac07c8e895d1e594767197755aa1d
FoxConN-M <victormenezes1121@gmail.com>
2025-09-13 20:50:33 -0300

    alphabetic order turned off

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,10 @@ from .api import bp as api_bp
 
 def create_app() -> Flask:
     app = Flask(__name__)
+    
+    app.json.sort_keys=False
+    app.config["JASON_SORT_KEYS"] = False
+
     app.register_blueprint(api_bp)
     return app
 
